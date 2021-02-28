c6131bb269cd094694c6f342959a912acbb663fb
BaichorovNory <62794751+BaichorovNory@users.noreply.github.com>
2021-03-01 01:44:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import React, { Suspense } from 'react';
-import { connect } from 'react-redux'
-import { Route, withRouter } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import { initializeApp } from './Store/appReducer';
+import { Route, withRouter } from 'react-router-dom';
 import Preloader from './components/Preloader/Preloader';
 import HeaderContainer from './components/Header/HeaderContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
 const News = React.lazy(() => import('./components/News/News'));
 const Login = React.lazy(() => import('./components/Login/Login'));
 const Music = React.lazy(() => import('./components/Music/Music'));
 const Settings = React.lazy(() => import('./components/Settin/Settings'));
 const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 
 class App extends React.Component {
   componentDidMount() {
     this.props.initializeApp();
   }
-  
+
   render() {
     if (!this.props.initialized) return <Preloader />
     return (
@@ -26,13 +27,13 @@ class App extends React.Component {
         <HeaderContainer />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path='/login' render={() => { return <Suspense fallback={<div>Loading...</div>}><Login /></Suspense> }} />
-          <Route path='/news' render={() => { return <Suspense fallback={<div>Loading...</div>}> <News /> </Suspense> }} />
-          <Route path='/music' render={() => { return <Suspense fallback={<div>Loading...</div>}> <Music /> </Suspense> }} />
-          <Route path='/settings' render={() => { return <Suspense fallback={<div>Loading...</div>}> <Settings /> </Suspense> }} />
-          <Route path='/users' render={() => { return <Suspense fallback={<div>Loading...</div>}> <UsersContainer /> </Suspense> }} />
-          <Route path='/messages' render={() => { return <Suspense fallback={<div>Loading...</div>}> <DialogsContainer /> </Suspense> }} />
-          <Route path='/profile/:id?' render={() => { return <ProfileContainer /> }} />
+          <Route path='/login' render={() => { return <Suspense fallback={<Preloader />}><Login /></Suspense> }} />
+          <Route path='/news' render={() => { return <Suspense fallback={<Preloader />}> <News /> </Suspense> }} />
+          <Route path='/music' render={() => { return <Suspense fallback={<Preloader />}> <Music /> </Suspense> }} />
+          <Route path='/settings' render={() => { return <Suspense fallback={<Preloader />}> <Settings /> </Suspense> }} />
+          <Route path='/users' render={() => { return <Suspense fallback={<Preloader />}> <UsersContainer /> </Suspense> }} />
+          <Route path='/messages' render={() => { return <Suspense fallback={<Preloader />}> <DialogsContainer /> </Suspense> }} />
+          <Route path='/profile/:id?' render={() => { return <Suspense fallback={<Preloader />}><ProfileContainer /> </Suspense> }} />
         </div>
       </div>
     )
