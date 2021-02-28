bcd643028aecc6d40fac9c5744a4a91ae73b0421
BaichorovNory <62794751+BaichorovNory@users.noreply.github.com>
2021-03-01 01:37:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { Suspense } from 'react';
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import { initializeApp } from './Store/appReducer';
 import Preloader from './components/Preloader/Preloader';
@@ -10,7 +10,6 @@ import ProfileContainer from './components/Profile/ProfileContainer';
 const News = React.lazy(() => import('./components/News/News'));
 const Login = React.lazy(() => import('./components/Login/Login'));
 const Music = React.lazy(() => import('./components/Music/Music'));
-const Friends = React.lazy(() => import('./components/Friends/Friends'));
 const Settings = React.lazy(() => import('./components/Settin/Settings'));
 const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
@@ -30,7 +29,6 @@ class App extends React.Component {
           <Route path='/login' render={() => { return <Suspense fallback={<div>Loading...</div>}><Login /></Suspense> }} />
           <Route path='/news' render={() => { return <Suspense fallback={<div>Loading...</div>}> <News /> </Suspense> }} />
           <Route path='/music' render={() => { return <Suspense fallback={<div>Loading...</div>}> <Music /> </Suspense> }} />
-          <Route path='/friends' render={() => { return <Suspense fallback={<div>Loading...</div>}> <Friends /> </Suspense> }} />
           <Route path='/settings' render={() => { return <Suspense fallback={<div>Loading...</div>}> <Settings /> </Suspense> }} />
           <Route path='/users' render={() => { return <Suspense fallback={<div>Loading...</div>}> <UsersContainer /> </Suspense> }} />
           <Route path='/messages' render={() => { return <Suspense fallback={<div>Loading...</div>}> <DialogsContainer /> </Suspense> }} />
@@ -44,4 +42,4 @@ class App extends React.Component {
 const mapStateToProps = (state) => ({
   initialized: state.app.initialized
 })
-export default connect(mapStateToProps, { initializeApp })(App);
+export default compose(connect(mapStateToProps, { initializeApp }), withRouter)(App);
